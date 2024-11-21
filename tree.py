8f82a90aea3fec837b7bf31f3e168b1118d0c258
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2024-11-21 23:10:12 +0000

    Add files via upload

---
+++ tree.py
@@ -0,0 +1,45 @@
+a=input("give a file .txt to convert to .dat? ")
+b=a.replace(".txt",".dat")
+f1=open(a,"r")
+source=f1.read()
+f1.close()
+source=source.split("\n")
+d=""
+counter2=0
+stacks=[]
+for n in source:
+     counter=0
+     TTrue=True
+     if n.strip()!="":
+         while TTrue:
+             if n[counter]==" ":
+                 counter+=1
+             else:
+                 TTrue=False
+     lines=n.split("=")
+     if len(stacks)<=counter:
+         stacks=stacks+[lines[0].strip()]
+     else:
+         if len(stacks)>0:
+             TTrue=True
+             TTrue=len(stacks)>counter
+             while TTrue:
+                 __=stacks.pop()
+                 TTrue=len(stacks)>counter
+             stacks=stacks+[lines[0].strip()]
+             counter2=counter
+     l=0
+     for h in stacks:
+          if l==0: 
+              d=d+h
+          else:
+              d=d+"\\"+h
+          l+=1
+     if len(lines)>1:
+         d=d+line[1]+"\n"
+     else:
+         d=d+"\n"
+
+f1=open(b,"w")
+source=f1.write(d)
+f1.close()
